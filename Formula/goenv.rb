fabee95521790e300b9fe98da24d2c39b501fbbb
Shintaro Kaneko <kaneshin0120@gmail.com>
2016-08-16 13:56:40 +0000

    Bump up 0.4.0

--- Formula/goenv.rb
+++ Formula/goenv.rb
@@ -1,10 +1,10 @@
 class Goenv < Formula
   desc "Golang Version Management"
   homepage "https://github.com/kaneshin/goenv"
-  url "https://github.com/kaneshin/goenv/archive/0.3.0.tar.gz"
-  sha256 "2e8134ed0e55b791a665601c340f268e0011f165f28a126714c4f4ada47e053b"
+  url "https://github.com/kaneshin/goenv/archive/v0.4.0.tar.gz"
+  sha256 "39a6709018520c140ac574092857e487cbc884bd1f95bc04b2eec22e99a9fd28"
   head "https://github.com/kaneshin/goenv.git"
-  version "0.3.0"
+  version "0.4.0"
 
   def install
     prefix.install Dir["*"]
