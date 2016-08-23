24b9fa59b409bfa002b093abf77a6cc3c7260624
Shintaro Kaneko <kaneshin0120@gmail.com>
2016-08-23 03:29:20 +0000

    Bump up 0.4.2

--- Formula/goenv.rb
+++ Formula/goenv.rb
@@ -1,22 +1,22 @@
 class Goenv < Formula
   desc "Golang Version Management"
   homepage "https://github.com/kaneshin/goenv"
-  url "https://github.com/kaneshin/goenv/archive/v0.4.1.tar.gz"
-  sha256 "975bc70099bc70e70946adb85e96b3779388cbba6183e35b1aa64df07e143bc9"
+  url "https://github.com/kaneshin/goenv/archive/v0.4.2.tar.gz"
+  sha256 "92995d6bc24ab07c6626d311d485c60b5b1ffbf71c1bf874779f86761e2a7bb7"
   head "https://github.com/kaneshin/goenv.git"
-  version "0.4.1"
+  version "0.4.2"
 
   def install
     prefix.install Dir["*"]
   end
 
   def caveats; <<-EOS.undent
-    In order to use this build of goenv as your interactive shell,
-    it must be added the following lines to your profile:
-      export GOENV_ROOT=#{opt_prefix}
+    goenv stores data under ~/.goenv by default. If you absolutely need to
+    store everything under Homebrew's prefix, include this in your profile:
+      export GOENV_ROOT=#{var}/goenv
       export PATH="$GOENV_ROOT/bin:$PATH"
 
-    And also add the following lines to enable shims:
+    To enable shims and autocompletion add the following line to your profile:
       which goenv > /dev/null && eval "$(goenv init -)"
     EOS
   end
